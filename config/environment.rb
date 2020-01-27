a680ef1e18fe279936bb1b374c5b791b94847b9b
Andres <afre92@gmail.com>
2020-01-26 20:25:48 -0500

    domain added to smtp

--- config/environment.rb
+++ config/environment.rb
@@ -7,7 +7,7 @@ Rails.application.initialize!
 ActionMailer::Base.smtp_settings = {
   :user_name =>  ENV['SENDGRID_USERNAME'],
   :password => ENV['SENDGRID_PASSWORD'],
-  :domain => 'yourdomain.com',
+  :domain => 'https://kalo-email-collector.herokuapp.com/',
   :address => 'smtp.sendgrid.net',
   :port => 465,
   :authentication => :plain,
