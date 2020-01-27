edff0fc449f52104386fa5db1d18f96f002c21a2
Andres <afre92@gmail.com>
2020-01-26 20:15:36 -0500

    alerts and notify via email added

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,20 @@
+class UsersController < ApplicationController
+
+  def create
+    @user = User.create(user_params)
+
+    if @user.save
+      UserNotifierMailer.send_signup_email(@user).deliver
+      redirect_to('/', notice: "Sign up Succesfull #{@user.first_name}, Please check your email for promotion code")
+    else
+      redirect_to('/', warning: 'Oops something went wrong!')
+    end
+  
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email)
+  end
+end
